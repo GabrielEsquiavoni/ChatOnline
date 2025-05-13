76c8d5b9b8512be6b9d7d4bbabec9cf5f522d4fa
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-05-13 16:06:04 -0300

    Add CORS support e authCheck no frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,4 +1,5 @@
 import {create} from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -7,4 +8,19 @@ export const useAuthStore = create((set) => ({
     isUpdatingProfile: false,
 
     isCheckingAuth: true,
+    
+    checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data });
+      get().connectSocket();
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 }))
+
