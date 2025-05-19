2463b955c7c221fc3ec149800504f280b741313f
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-05-19 16:11:37 -0300

    Configurações de Tema

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 
-import { Routes, Route, Navigate } from 'react-router-dom'; // <- CORRIGIDO AQUI
+import { Routes, Route, Navigate } from 'react-router-dom'; 
 import { useAuthStore } from './store/useAuthStore.js';
 import { useEffect } from 'react';
 
